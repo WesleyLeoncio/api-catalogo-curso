8d4d28085dcf32de90b25ef8fa4747e076bfce85
Your Name <wesley.leoncio.izi@gmail.com>
2024-05-29 18:09:22 -0300

    refactor: Pequena modificação

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using api_catalogo_curso.infra.data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//TODO SE DER ERRO DE CYCLONISAÇÃO
+// builder.Services.AddControllers()
+//     .AddJsonOptions(option => option.JsonSerializerOptions
+//         .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- modules/categoria/controller/CategoriaController.cs
+++ modules/categoria/controller/CategoriaController.cs
@@ -5,6 +5,6 @@ namespace api_catalogo_curso.modules.categoria.controller;
 [ApiController]
 [Route("[controller]")]
 public class CategoriaController : ControllerBase
-{   
- 
+{
+    
 }
